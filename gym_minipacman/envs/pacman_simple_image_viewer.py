c58d700b39d5d9a0b64cf8dd813b2e840bdc977a
Florian Klemt <FKlemt@gmx.de>
2018-05-05 18:53:55 +0200

    Fixed bug with minipacman when running on a server without a display

--- gym_minipacman/envs/pacman_simple_image_viewer.py
+++ gym_minipacman/envs/pacman_simple_image_viewer.py
@@ -6,10 +6,10 @@ try:
 except ImportError as e:
     reraise(suffix="HINT: you can install pyglet directly via 'pip install pyglet'. But if you really just want to install all Gym dependencies and not have to think about it, 'pip install -e .[all]' or 'pip install gym[all]' will do it.")
 
-try:
-    from pyglet.gl import *
-except ImportError as e:
-    reraise(prefix="Error occured while running `from pyglet.gl import *`",suffix="HINT: make sure you have OpenGL install. On Ubuntu, you can run 'apt-get install python-opengl'. If you're running on a server, you may need a virtual frame buffer; something like this should work: 'xvfb-run -s \"-screen 0 1400x900x24\" python <your_script.py>'")
+#try:
+#    from pyglet.gl import *
+#except ImportError as e:
+#    reraise(prefix="Error occured while running `from pyglet.gl import *`",suffix="HINT: make sure you have OpenGL install. On Ubuntu, you can run 'apt-get install python-opengl'. If you're running on a server, you may need a virtual frame buffer; something like this should work: 'xvfb-run -s \"-screen 0 1400x900x24\" python <your_script.py>'")
 
 
 class Pacman_SimpleImageViewer(object):
@@ -19,6 +19,8 @@ class Pacman_SimpleImageViewer(object):
         self.display = display
         self.scale = scale
     def imshow(self, arr):
+        from pyglet.gl import window	#importing here instead of outside this method allows minipacman to run without a display (eg. on a server)
+
         #import time
         #time.sleep(1)
         height, width, _channels = arr.shape
